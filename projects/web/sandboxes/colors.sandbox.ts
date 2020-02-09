5709b0f078cfbbfb8f48b3310b801b4f1c9eeaae
mehmet-erim <bilgi@mehmeterim.com>
2020-02-09 17:46:21 +0300

    feat: update colors sandbox

--- projects/web/sandboxes/colors.sandbox.ts
+++ projects/web/sandboxes/colors.sandbox.ts
@@ -1,16 +1,34 @@
-import { sandboxOf } from "angular-playground";
-import { Component } from "@angular/core";
+import { sandboxOf } from 'angular-playground';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-colors",
+  selector: 'app-colors',
   template: `
-    app-colors
-  `
+    <div class="box bg-primary">primary</div>
+    <div class="box bg-secondary">secondary</div>
+    <div class="box bg-success">success</div>
+    <div class="box bg-info">info</div>
+    <div class="box bg-warning">warn</div>
+    <div class="box bg-dark">dark</div>
+    <div class="box bg-light text-dark">light</div>
+  `,
+  styles: [
+    `
+      .box {
+        height: 100px;
+        width: 100px;
+        margin: 10px;
+        text-align: center;
+        display: inline-block;
+        color: white;
+      }
+    `,
+  ],
 })
-export class ColorsComponent {
+class ColorsComponent {
   constructor() {}
 }
 
-export default sandboxOf(ColorsComponent).add("Colors", {
-  template: `<div>Hey</div>`
+export default sandboxOf(ColorsComponent).add('Colors', {
+  template: `<app-colors></app-colors>`,
 });
